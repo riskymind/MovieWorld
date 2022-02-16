3ae71f452bd5140c536a5d2be21ef510752ef62a
kelechi <oparaiskelechi@gmail.com>
2022-02-16 13:10:53 +0100

    setup the navigation component and acreens

---
+++ app/src/main/java/com/asterisk/movieworld/ui/now_playing/NowPlayingFragment.kt
@@ -0,0 +1,13 @@
+package com.asterisk.movieworld.ui.now_playing
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.asterisk.movieworld.R
+
+
+class NowPlayingFragment : Fragment(R.layout.fragment_now_playing) {
+
+}
---
+++ app/src/main/java/com/asterisk/movieworld/ui/saves/SaveMoviesFragment.kt
@@ -0,0 +1,12 @@
+package com.asterisk.movieworld.ui.saves
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.asterisk.movieworld.R
+
+class SaveMoviesFragment : Fragment(R.layout.fragment_save_movies) {
+
+}
